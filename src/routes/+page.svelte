9488e4bffb76a30223d366284b59cb65ed25e8e2
Isaac Druin <isaac.druin@gmail.com>
2023-12-10 11:20:41 -0600

    render on mount

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,20 +1,12 @@
-<script>
+<script lang="ts">
 	import mermaid from 'mermaid';
+	import { onMount } from 'svelte';
+
 	const kph = 34;
-</script>
-
-<div>
-	<p>A demo of how a TT Gantt chart might look.</p>
-	<p>Kits per hour: {kph}</p>
-</div>
-
-<pre class="mermaid">
-    ---
-    displayMode: compact
-    ---
+	let diagram = `\
+  
     gantt
     tickInterval 1hour
-
     title Truxport Line 1
     axisFormat %
     Section RUSH
@@ -22,23 +14,45 @@
     section TT A
         
     section TT B
-        B 01 - 06  :b01, after g01, {64 / kph}h
-        B 07 - 34  :b02, after h01, {334 / kph}h
+        B 01 - 06  :b01, after g01, ${64 / kph}h
+        B 07 - 34  :b02, after h01, ${334 / kph}h
     section TT C
         
     section TT D
-        D 07 - 34 :d1, 2023-12-10, {328 / kph}h
+        D 07 - 34 :d1, 2023-12-10, ${328 / kph}h
     section TT E
-        E 01 - 02 :e01, after d1, {24 / kph}h
+        E 01 - 02 :e01, after d1, ${24 / kph}h
     section TT F
-        F 01      :f01, after e01, {1 / kph}h
+        F 01      :f01, after e01, ${1 / kph}h
     section TT G
-        G 18 - 35 :g01, after x01, {210 / kph}h
+        G 18 - 35 :g01, after x01, ${210 / kph}h
     section TT H
-        H 01 - 34 :h01, after g01, {408 / kph}h
+        H 01 - 34 :h01, after g01, ${408 / kph}h
     section TT X
-        X 01 - 02 :x01, after f01, {36 / kph}h
-</pre>
+        X 01 - 02 :x01, after f01, ${36 / kph}h
+    `;
+
+	let container: HTMLElement;
+
+	async function renderDiagram() {
+		// console.log(diagram);
+		const { svg } = await mermaid.render('mermaid', diagram);
+		container.innerHTML = svg;
+	}
+
+	onMount(() => {
+		renderDiagram();
+	});
+</script>
+
+<main>
+	<div>
+		<p>A demo of how a TT Gantt chart might look.</p>
+		<p>Kits per hour: {kph}</p>
+	</div>
+	<!-- <pre contenteditable="true" bind:innerHTML={diagram}></pre> -->
+	<span bind:this={container}></span>
+</main>
 
 <style global>
 	.grid .tick {
